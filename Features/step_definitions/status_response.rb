b616ee7877309cb5b2e727f9d2a7ba8edb88fbb1
Tinashe Madzingaidzo <tinashe.madzingaidzo@hetzner.co.za>
2016-01-25 17:01:33 +0200

    Created a step definition folder and the skeleton for the steps for send_status_response.feature

---
+++ Features/step_definitions/status_response.rb
@@ -0,0 +1,31 @@
+Given(/^functional monitoring agent$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^functional status$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the functional monitoring agent queries my functional status$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I send a response indicating the functional status to the functional monitoring agent$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^no service$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I send a "([^"]*)" notification$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^functional status can not be determined$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I send "([^"]*)" notification$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
